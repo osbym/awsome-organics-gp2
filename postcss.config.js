4c575b3c950a521ad1824b3100044ca7bb05a2ff
HacAtac <jhack00@icloud.com>
2021-11-16 08:29:21 -0600

    file structure for public/views and made main.handlebars for landing page

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+//postcss.config.js
+//this will be used by postcss-loader to load the postcss.config.js file in the postcss-loader package
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
