db5d3acc71a43fd57624985cd27eafafd37e9ac3
rvanooyen <robert.vanooyen@gmail.com>
2021-11-17 21:35:49 -0600

    update

--- models/Customer.js
+++ models/Customer.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isEmail();
+        isEmail: true
       }
     }
   });
