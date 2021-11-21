26a790039a16dc8f0a2f212ec502e3b8f078226d
kumikoyamagata <sun.kumiko@protonmail.com>
2021-11-21 06:34:41 -0600

    edited seven photo image files

--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS project-two_db;
-
-CREATE DATABASE project-two_db;
--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-//postcss.config.js
-//this will be used by postcss-loader to load the postcss.config.js file in the postcss-loader package
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
