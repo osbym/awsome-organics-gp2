024601c8e0b4cc7f6acf47b8ae9fe4c33a40a076
HacAtac <jhack00@icloud.com>
2021-11-30 14:19:34 -0600

    changed file structure to get routes/server working

---
+++ controllers/api/productsroutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const { Product } = require("../models/");
+
+//TODO create product route 'post route'
+
+//TODO update product route 'put route'
+
+//TODO delete product route 'delet route'
+
+module.exports = router;
---
+++ controllers/homeroutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { Product, User } = require("../models/");
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+//need to add get from product table
+router.get("/products", (req, res) => {
+  res.render("products");
+});
+
+router.get("/contact_me", (req, res) => {
+  res.render("contact");
+});
+
+router.get("/login", (req, res) => {
+  res.render("registration");
+});
+
+//TODO after login
+
+module.exports = router;
