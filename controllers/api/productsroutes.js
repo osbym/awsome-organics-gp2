9dd89bf59e206fee7189e7ffc91ea5e3d4ec56d4
HacAtac <jhack00@icloud.com>
2021-11-30 17:31:05 -0600

    added CRUD http methods for users and products

--- controllers/api/productsroutes.js
+++ controllers/api/productsroutes.js
@@ -1,10 +1,48 @@
 const router = require("express").Router();
 const { Product } = require("../models/");
 
-//TODO create product route 'post route'
+//TODO create product route 'post route
 
+router.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).send(product);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 //TODO update product route 'put route'
+router.put("/api/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    if (product) {
+      const updatedProduct = await product.update(req.body);
+      res.status(200).send(updatedProduct);
+    } else {
+      res.status(404).send({
+        message: "Product not found",
+      });
+    }
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 //TODO delete product route 'delet route'
+router.delete("/api/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    if (product) {
+      const deletedProduct = await product.destroy();
+      res.status(200).send(deletedProduct);
+    } else {
+      res.status(404).send({
+        message: "Product not found",
+      });
+    }
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 module.exports = router;
