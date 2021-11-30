9934a40df74311252038832634f7a2446cf72a2d
HacAtac <jhack00@icloud.com>
2021-11-30 16:23:08 -0600

    latest update

--- routes/application.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const application_controller = require("../controllers/application_controller");
-
-router.get("/", application_controller.index); //what is index in here ? // index is a function in application_controller.js
-//will be called when the user goes to the root of the application (localhost:3000/)
-
-module.exports = router;
--- routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const application_controller = require("../controllers/application_controller");
-
-router.get("/products", application_controller.products); //this is the route for the products page //products is a function in products_controller.js
-//then we will call the products function in products_controller.js
-
-module.exports = router;
