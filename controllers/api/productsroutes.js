4953a166fb6ff06b473391d42cdd1569c499bf73
HacAtac <jhack00@icloud.com>
2021-11-30 21:48:26 -0600

    updated code

--- controllers/api/productsroutes.js
+++ controllers/api/productsroutes.js
@@ -5,7 +5,8 @@ const { Product } = require("../models/");
 
 router.post("/api/products", async (req, res) => {
   try {
-    const product = await Product.create(req.body);
+    const product = await Product.create(req.body); //this will create a new product
+
     res.status(201).send(product);
   } catch (err) {
     res.status(400).send(err);
@@ -14,7 +15,10 @@ router.post("/api/products", async (req, res) => {
 //TODO update product route 'put route'
 router.put("/api/products/:id", async (req, res) => {
   try {
-    const product = await Product.findByPk(req.params.id);
+    const product = await Product.findByPk(req.params.id); //this is the id of the product that we are trying to update
+    //then we are trying to update the product with the id that we are trying to update
+    //if the product exists then we are going to update it with the new information that we are trying to update it with
+    //if the product does not exist then we are going to send a 404 error
     if (product) {
       const updatedProduct = await product.update(req.body);
       res.status(200).send(updatedProduct);
