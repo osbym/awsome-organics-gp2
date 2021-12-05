b0cc1bc1d3e9d4b56c33b135d6d906bbf141d2a9
HacAtac <jhack00@icloud.com>
2021-12-05 14:46:53 -0600

    hmm

--- server.js
+++ server.js
@@ -110,9 +110,9 @@ const sess = {
 };
 //connection to DB and setting up handlebars
 app.use(session(sess));
-const hbs = exphbs.create({ helpers });
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+const hbs = exphbs.create({ helpers }); //creates handlebars
+app.engine("handlebars", hbs.engine); // sets up handlebars
+app.set("view engine", "handlebars"); //sets up handlebars
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
